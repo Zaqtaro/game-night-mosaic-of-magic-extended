8252114635a2b1f9c3c86e4ac7856078ccc507eb
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-11-19 21:04:44 -0500

    further name fixes

--- Contents/mods/inventor-legacy/media/lua/client/gameNight - implementMoMEx - InventorLegacy.lua
+++ Contents/mods/inventor-legacy/media/lua/client/gameNight - implementMoMEx - InventorLegacy.lua
@@ -41,20 +41,20 @@ MOM.sets["Inventor's Legacy"] = {
 
 
 	rarities = {
-        	Rare = {"White 1", "Blue 3", "Black 7", "Red 7", "Green 5", "Relic 7", "Relic 8", "Relic 10",
-					 "Relic 11", "Relic 12", "Relic 16", "Relic 22", "Relic 23", "Relic 29", "Relic 33",
-					 "Relic 37", "Relic 39", "Relic 40", "Relic 42", "domain 2",},
+        	Rare = {"White 1", "Blue 3", "Black 7", "Red 7", "Green 5", "Relics 7", "Relics 8", "Relics 10",
+					 "Relics 11", "Relics 12", "Relics 16", "Relics 22", "Relics 23", "Relics 29", "Relics 33",
+					 "Relics 37", "Relics 39", "Relics 40", "Relics 42", "domain 2",},
 
 		Uncommon = {"White 4", "White 5", "White 6", "Blue 2", "Blue 4", "Blue 7", "Black 2", "Black 3", "Black 6",
-					"Red 3", "Red 4", "Red 5", "Green 3", "Green 6", "Green 7", "Relic 2", "Relic 3", "Relic 4",
-					"Relic 5", "Relic 13", "Relic 15", "Relic 18", "Relic 19", "Relic 20", "Relic 21", "Relic 24",
-					"Relic 26", "Relic 27", "Relic 28", "Relic 31", "Relic 34", "Relic 35", "Relic 38", "Relic 43",
-					"Relic 44", "Domain 1", "Domain 3", "Domain 4", },
+					"Red 3", "Red 4", "Red 5", "Green 3", "Green 6", "Green 7", "Relics 2", "Relics 3", "Relics 4",
+					"Relics 5", "Relics 13", "Relics 15", "Relics 18", "Relics 19", "Relics 20", "Relics 21", "Relics 24",
+					"Relics 26", "Relics 27", "Relics 28", "Relics 31", "Relics 34", "Relics 35", "Relics 38", "Relics 43",
+					"Relics 44", "Domain 1", "Domain 3", "Domain 4", },
 
 		Common = {"White 2", "White 3", "White 7", "Blue 1", "Blue 5", "Blue 6", "Black 1", "Black 4", "Black 5",
-				  "Red 1", "Red 2", "Red 6", "Green 1", "Green 2", "Green 4", "Relic 1", "Relic 6", "Relic 9",
-				  "Relic 14", "Relic 17", "Relic 25", "Relic 30", "Relic 32", "Relic 41", "Relic 45", "domain 5",
-				  "domain 6"},
+				  "Red 1", "Red 2", "Red 6", "Green 1", "Green 2", "Green 4", "Relics 1", "Relics 6", "Relics 9",
+				  "Relics 14", "Relics 17", "Relics 25", "Relics 30", "Relics 32", "Relics 41", "Relics 45", "domain 5",
+				  "Domain 6"},
 
         Domain = { "Red Domain 1", "Blue Domain 1", "Green Domain 1", "White Domain 1", "Black Domain 1", },
     },
@@ -68,15 +68,15 @@ MOM.preBuiltSets.ILAll = {
 			  "Black 1", "Black 2", "Black 3", "Black 4", "Black 5", "Black 6", "Black 7",
 			  "Red 1", "Red 2", "Red 3", "Red 4", "Red 5", "Red 6", "Red 7",
 			  "Green 1", "Green 2", "Green 3", "Green 4", "Green 5", "Green 6", "Green 7",
-			  "Relic 1", "Relic 2", "Relic 3", "Relic 4", "Relic 5", "Relic 6", "Relic 7",
-			  "Relic 8", "Relic 9", "Relic 10", "Relic 11", "Relic 12", "Relic 13", "Relic 14",
-			  "Relic 15", "Relic 16", "Relic 17", "Relic 18", "Relic 19", "Relic 20", "Relic 21",
-			  "Relic 22", "Relic 23", "Relic 24", "Relic 25", "Relic 26", "Relic 27", "Relic 28",
-			  "Relic 29", "Relic 30", "Relic 31", "Relic 32", "Relic 33", "Relic 34", "Relic 35",
-			  "Relic 36", "Relic 37", "Relic 38", "Relic 39", "Relic 40", "Relic 41", "Relic 42",
-			  "Relic 43", "Relic 44", --"Relic 45", no 45 found
+			  "Relics 1", "Relics 2", "Relics 3", "Relics 4", "Relics 5", "Relics 6", "Relics 7",
+			  "Relics 8", "Relics 9", "Relics 10", "Relics 11", "Relics 12", "Relics 13", "Relics 14",
+			  "Relics 15", "Relics 16", "Relics 17", "Relics 18", "Relics 19", "Relics 20", "Relics 21",
+			  "Relics 22", "Relics 23", "Relics 24", "Relics 25", "Relics 26", "Relics 27", "Relics 28",
+			  "Relics 29", "Relics 30", "Relics 31", "Relics 32", "Relics 33", "Relics 34", "Relics 35",
+			  "Relics 36", "Relics 37", "Relics 38", "Relics 39", "Relics 40", "Relics 41", "Relics 42",
+			  "Relics 43", "Relics 44", --"Relics 45", no 45 found
 			  "Domain 1", "Domain 2", "Domain 3", "Domain 4", "Domain 5", "Domain 6",
 			  "Red Domain 1", "Blue Domain 1", "Green Domain 1", "White Domain 1", "Black Domain 1", },
 
-    set = "Inventor’s Legacy"
+    set = "Inventor's Legacy"
 }
