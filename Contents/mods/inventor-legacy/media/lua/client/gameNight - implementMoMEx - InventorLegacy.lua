ddc09881be2c515d0dbe946818b12fa5f720b80f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-11-19 20:46:56 -0500

    file name issues

--- Contents/mods/inventor-legacy/media/lua/client/gameNight - implementMoMEx - InventorLegacy.lua
+++ Contents/mods/inventor-legacy/media/lua/client/gameNight - implementMoMEx - InventorLegacy.lua
@@ -41,18 +41,18 @@ MOM.sets["Inventor's Legacy"] = {
 
 
 	rarities = {
-        	Rare = {"white 1", "blue 3", "black 7", "red 7", "green 5", "Relic 7", "Relic 8", "Relic 10",
+        	Rare = {"White 1", "Blue 3", "Black 7", "Red 7", "Green 5", "Relic 7", "Relic 8", "Relic 10",
 					 "Relic 11", "Relic 12", "Relic 16", "Relic 22", "Relic 23", "Relic 29", "Relic 33",
 					 "Relic 37", "Relic 39", "Relic 40", "Relic 42", "domain 2",},
 
-		Uncommon = {"white 4", "white 5", "white 6", "blue 2", "blue 4", "blue 7", "black 2", "black 3", "black 6",
-					"red 3", "red 4", "red 5", "green 3", "green 6", "green 7", "Relic 2", "Relic 3", "Relic 4",
+		Uncommon = {"White 4", "White 5", "White 6", "Blue 2", "Blue 4", "Blue 7", "Black 2", "Black 3", "Black 6",
+					"Red 3", "Red 4", "Red 5", "Green 3", "Green 6", "Green 7", "Relic 2", "Relic 3", "Relic 4",
 					"Relic 5", "Relic 13", "Relic 15", "Relic 18", "Relic 19", "Relic 20", "Relic 21", "Relic 24",
 					"Relic 26", "Relic 27", "Relic 28", "Relic 31", "Relic 34", "Relic 35", "Relic 38", "Relic 43",
 					"Relic 44", "Domain 1", "Domain 3", "Domain 4", },
 
-		Common = {"white 2", "white 3", "white 7", "blue 1", "blue 5", "blue 6", "black 1", "black 4", "black 5",
-				  "red 1", "red 2", "red 6", "green 1", "green 2", "green 4", "Relic 1", "Relic 6", "Relic 9",
+		Common = {"White 2", "White 3", "White 7", "Blue 1", "Blue 5", "Blue 6", "Black 1", "Black 4", "Black 5",
+				  "Red 1", "Red 2", "Red 6", "Green 1", "Green 2", "Green 4", "Relic 1", "Relic 6", "Relic 9",
 				  "Relic 14", "Relic 17", "Relic 25", "Relic 30", "Relic 32", "Relic 41", "Relic 45", "domain 5",
 				  "domain 6"},
 
@@ -63,11 +63,11 @@ MOM.sets["Inventor's Legacy"] = {
 
 MOM.preBuiltSets.ILAll = {
     
-    cards = { "white 1", "white 2", "white 3", "white 4", "white 5", "white 6", "white 7",
-			  "blue 1", "blue 2", "blue 3", "blue 4", "blue 5", "blue 6", "blue 7",
-			  "black 1", "black 2", "black 3", "black 4", "black 5", "black 6", "black 7",
-			  "red 1", "red 2", "red 3", "red 4", "red 5", "red 6", "red 7",
-			  "green 1", "green 2", "green 3", "green 4", "green 5", "green 6", "green 7",
+    cards = { "White 1", "White 2", "White 3", "White 4", "White 5", "White 6", "White 7",
+			  "Blue 1", "Blue 2", "Blue 3", "Blue 4", "Blue 5", "Blue 6", "Blue 7",
+			  "Black 1", "Black 2", "Black 3", "Black 4", "Black 5", "Black 6", "Black 7",
+			  "Red 1", "Red 2", "Red 3", "Red 4", "Red 5", "Red 6", "Red 7",
+			  "Green 1", "Green 2", "Green 3", "Green 4", "Green 5", "Green 6", "Green 7",
 			  "Relic 1", "Relic 2", "Relic 3", "Relic 4", "Relic 5", "Relic 6", "Relic 7",
 			  "Relic 8", "Relic 9", "Relic 10", "Relic 11", "Relic 12", "Relic 13", "Relic 14",
 			  "Relic 15", "Relic 16", "Relic 17", "Relic 18", "Relic 19", "Relic 20", "Relic 21",
